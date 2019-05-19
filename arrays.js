705b3a846106453496567101cc0c4410e98187cb
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 04:38:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  [elemet, ...array];
+  array = [elemet, ...array];
   return array;
 }
 
