6dc5ea042080abc45e924496acd3a2b9182f8a81
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 04:37:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-var array = new Array();
+var array = [];
 function addElementToBeginningOfArray(array, element) {
   [elemet, ...array];
   return array;
