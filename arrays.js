34904e9ffdaab0438c23b1c63b6cfda2c5819520
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 04:28:50 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,10 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 console.log(addElementToBeginningOfArray(chocolateBars, "foo"));
+
+function addElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
+
+console.log(addElementToBeginningOfArray(chocolateBars, "foo"));
